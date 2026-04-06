55006296297eddb1a123583d684613d50d100738
Mariana Dos Santos Lourenço <marianalourencosd@gmail.com>
2026-04-06 17:40:33 -0300

    Adicionando as imagens no projeto

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,11 +3,9 @@ import "./Home.css";
 
 function Home() {
   return (
-    <div className="container text-center mt-5">
-      <h1 className="text-primary">Home</h1>
-      <p className="lead">Bem-vindo ao projeto React + Vite + Bootstrap</p>
-      <button className="btn btn-success">Clique aqui</button>
-    </div>
+    <>
+     
+    </>
   );
 }
 
